0bdcf1652d47a55e3bdedc3ddb55a64b9c97f7ca
JuShapiro <yuliashepel19@gmail.com>
2024-05-10 18:32:42 +0200

    not work

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,32 @@
+import css from "./Profile.module.css";
+console.log(css);
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className={css["user-card"]}>
+      <div className={css["user-profile"]}>
+        <img className={css["user-photo"]} src={image} alt="User avatar" />
+        <p className={css["user-name"]}>{name}</p>
+        <p className={css["user-info"]}>@{tag}</p>
+        <p className={css["user-info"]}>{location}</p>
+      </div>
+
+      <ul className={css["user-card-list"]}>
+        <li className={css["user-card-list-item"]}>
+          <span className={css["user-rating"]}>Followers</span>
+          <span className={css["user-stats"]}>{stats}</span>
+        </li>
+        <li className={css["user-card-list-item"]}>
+          <span className={css["user-rating"]}>Views</span>
+          <span className={css["user-stats"]}>{stats}</span>
+        </li>
+        <li className={css["user-card-list-item"]}>
+          <span className={css["user-rating"]}>Likes</span>
+          <span className={css["user-stats"]}>{stats}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
