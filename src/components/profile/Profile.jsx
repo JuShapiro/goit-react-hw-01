2772464d418562fa5114edc339680b147a33bbfc
JuShapiro <yuliashepel19@gmail.com>
2024-05-10 18:43:57 +0200

    work-but-fix-css

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -14,15 +14,15 @@ const Profile = ({ name, tag, location, image, stats }) => {
       <ul className={css["user-card-list"]}>
         <li className={css["user-card-list-item"]}>
           <span className={css["user-rating"]}>Followers</span>
-          <span className={css["user-stats"]}>{stats}</span>
+          <span className={css["user-stats"]}>{stats.followers}</span>
         </li>
         <li className={css["user-card-list-item"]}>
           <span className={css["user-rating"]}>Views</span>
-          <span className={css["user-stats"]}>{stats}</span>
+          <span className={css["user-stats"]}>{stats.views}</span>
         </li>
         <li className={css["user-card-list-item"]}>
           <span className={css["user-rating"]}>Likes</span>
-          <span className={css["user-stats"]}>{stats}</span>
+          <span className={css["user-stats"]}>{stats.likes}</span>
         </li>
       </ul>
     </div>
